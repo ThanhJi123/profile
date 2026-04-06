5fc6f56102ee7b079edc70cddabe23daee567233
ThanhJi123 <thanhvy3452@gmail.com>
2026-04-07 00:50:04 +0700

    add music

--- song.js
+++ song.js
@@ -1,39 +1,39 @@
 let songs = [
     {
-        name:'My computer',
-        path:'nhac/tinh_thoi_xot_xa_rm.mp3',
-        artist:'',
-        image:'anh/anh_1.jpg'
+        name: 'My computer',
+        path: 'nhac/tinh_thoi_xot_xa_rm.mp3',
+        artist: '',
+        image: 'anh/anh_1.jpg'
     },
     {
-        name:'khi ma',
-        path:'nhac/KhiMa1-Ronboogz-8391942.mp3',
-        artist:'Ronboogz',
-        image:'anh/anh_khi_ma.jpg'
+        name: 'khi ma',
+        path: 'nhac/KhiMa1-Ronboogz-8391942.mp3',
+        artist: 'Ronboogz',
+        image: 'anh/anh_khi_ma.jpg'
     },
     {
-        name:'Khong the say',
-        path:'nhac/KhongTheSay.mp3',
-        artist:'HIEUTHUHAI',
-        image:'anh/anh_khong_the_say.jpg'
+        name: 'Khong the say',
+        path: 'nhac/KhongTheSay.mp3',
+        artist: 'HIEUTHUHAI',
+        image: 'anh/anh_khong_the_say.jpg'
     },
     {
-        name:'them bao nhieu lau',
-        path:'nhac/them_bao_nhieu_lau.mp3',
-        artist:'Đạt G',
-        image:'anh/them_bao_nhieu_lau.jpg'
+        name: 'them bao nhieu lau',
+        path: 'nhac/them_bao_nhieu_lau.mp3',
+        artist: 'Đạt G',
+        image: 'anh/them_bao_nhieu_lau.jpg'
     },
     {
-        name:'anh tu do nhung co don',
-        path:'nhac/anh_tu_do_nhung_co_don.mp3',
-        artist:'Đạt G',
-        image:'anh/anh_tu_do_nhung_co_don.jpg'
+        name: 'anh tu do nhung co don',
+        path: 'nhac/anh_tu_do_nhung_co_don.mp3',
+        artist: 'Đạt G',
+        image: 'anh/anh_tu_do_nhung_co_don.jpg'
     },
     {
-        name:'Thien An oi !!!',
-        path:'nhac/thien_an.mp3',
-        artist:'JACK',
-        image:'anh/anh_thien_an.jpg'
+        name: 'Thien An oi !!!',
+        path: 'nhac/thien_an.mp3',
+        artist: 'JACK',
+        image: 'anh/anh_thien_an.jpg'
     },
 
 ];
